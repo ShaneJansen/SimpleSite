d997e334f3deacf88f67b099736eb76ecb554176
Shane Jansen <shanejjansen@gmail.com>
2016-04-23 21:51:38 -0400

    Added title.

--- app/view/html/includes/header.php
+++ app/view/html/includes/header.php
@@ -3,6 +3,7 @@
 <html>
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>KACADA</title>
 
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
